1486e04448bbf8e629a4d688c6dc9cf2e046e896
Justin Michaliga <justin.michaliga@gmail.com>
2013-09-11 16:14:47 -0400

    header/navigation includes

---
+++ _includes/header.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+ <head>
+  	<meta charset="utf-8">
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- SEO CHANGE -->
+  	<title>t h e o r y</title>
+  	<meta name="description" content="">
+  	<meta name="author" content="">
+    <meta name="keywords" content="" />
+  	
+  	<meta property="og:image" content=""/>
+  	<meta property="og:title" content=""/>
+  	<meta property="og:url" content=""/>
+  	<meta property="og:site_name" content=""/>
+  	<meta property="og:type" content=""/>
+
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/theory144.jpg">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/theory114.jpg">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/theory72.jpg">
+    <link rel="apple-touch-icon-precomposed" href="img/theory50.jpg">
+
+    <link rel="stylesheet" href="css/normalize.css">
+<!--     <link rel="stylesheet" href="http://code.jquery.com/ui/1.7.1/themes/smoothness/jquery-ui.css"> -->
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/layout.css">
+
+
+<!--[if gte IE 7]>
+    <link rel="stylesheet" type="text/css" href="css/ie7plus.css" />
+<![endif]-->
+
+    <link href="http://fnt.webink.com/wfs/webink.css/?project=BB1D4B98-17C2-4D73-AD9B-9639CBE2297A&fonts=F77BBDE3-5270-5846-90AD-5529C2FFDA57:f=Theinhardt-Medium,1C9C6E73-C362-ABF4-C5DC-EE735D774137:f=Genath-LightIta,E03E8258-216D-FA88-0D87-928C28562468:f=Genath-Light,8434466A-23DD-9CCA-882C-32A07E66E747:f=Genath-RegularIta,7AB8E18F-22A6-B196-88DB-03846F960FDF:f=Genath-Bold,864889ED-8E73-7E19-00E2-BBE0F997E58C:f=Theinhardt-Thin,008579D7-00D8-1E34-1306-843EC6BC82EA:f=Theinhardt-Light,7B3812DA-4DA8-9D05-72E2-DE755A973015:f=Genath-Regular" rel="stylesheet" type="text/css"/>
+
+    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+ </head>
+
+ <body>
+  <!--[if lt IE 7]>
+  	<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+  <![endif]-->
+
+
+    <!-- 
+
+    Start of Header:  Top row consists of 24 rows of 12-12. Logo spans 24 rows and is centered.
+    Shopping bag is relatively positioned under the shopping cart & is also customized to show on toggle 
+
+    -->
+
+  	<header id="header" class="container_24">
+
+      <div class="grid_12">
+          <h6 id="returns"><a href="">Free Shipping and Returns</a></h6>
+          <h6 id="region">United States (<a href="">Change</a>)</h6>
+      </div>
+      <!-- end .grid_12 -->
+
+      <div class="grid_12 text_right">
+          <h6 id="sign_in"><a href="">Sign In</a></h6>
+          <h6 id="customer_service"><a href="">Customer Service</a></h6>
+          <h6 id="shopping_cart">
+            <a href="#">Shopping Bag <div id="bag"><span id="count">2</span></div></a>
+          </h6>
+      </div>
+      <!-- end .grid_12 -->
+
+      <div class="clear">&nbsp;</div>
+
+      <div id="logo" class="grid_24">
+        <a href>theory</a>
+      </div>
+
+      <div class="clear">&nbsp;</div>
+
+      <!-- Start of Shopping Bag. Note: Inline style to display: none at load -->
+
+      <div id="shopping_bag" style="display: none;">
+        <div class="container">
+            <div class="product">
+              <div class="product_thumb">
+                <img src="http://placehold.it/40x72"/>
+              </div>
+              <div class="product_desc">
+                  <p><i>Charelle</i> Top in double Georgette Silk</p>
+                  <p class="product_price">$195.00</p>
+              </div>
+            </div><!--/.product-->
+            <div class="clear">&nbsp;</div>
+            <div id="shopping_actions">
+                <button class="checkout">Checkout</button>
+                <button class="view">View/Edit Bag</button>
+            </div><!--/#shopping_actions-->
+        </div><!--/.container-->
+      </div><!--/#shopping_bag-->
+
+    </header>
+
---
+++ _includes/navigation.php
@@ -0,0 +1,95 @@
+    <!-- 
+
+    Start of Main Navigation: List elements that have added bottom padding via javascript on hover. 
+    The respective .dropdown is shown and consists of 24 rows of 9-3-3-5 with custom padding. 
+    Search element is also customized to show and animate input elements
+
+  -->
+
+    <nav id="navigation" class="container_24">
+
+        <ul id="main_nav">
+          <li id="nav_women" class="drop">
+            <a href="" class="nav_button">Women</a>
+            <div class="dropdown">
+
+                <div class="grid_9 nav_col">
+                  
+                  <img src="http://placehold.it/330x175"/>
+
+                </div><!-- end .grid_8 -->
+                <div class="grid_3 nav_col">
+                  <h5>Features</h5>
+                  <ul>
+                      <li><a href="">New Arrivals</a></li>
+                      <li><a href="">Best Sellers</a></li>
+                      <li><a href="">Our Picks for Fall</a></li>
+                      <li><a href="">The Lookbook</a></li>
+                      <li><strong><a href="">Sale</a></strong></li>
+                  </ul>
+                </div><!-- end .grid_5 -->
+                <div class="grid_3 nav_col">
+                  <h5>Brands</h5>
+                  <ul>
+                      <li><a href="">Theory Classics</a></li>
+                      <li><a href="">Theory 38</a></li>
+                      <li><a href="">Icon</a></li>
+                      <li><a href="">Theysken's Theory</a></li>
+                      <li><strong><a href="">Shop All</a></strong></li>
+                  </ul>
+                </div><!-- end .grid_5 -->
+                <div class="grid_5 nav_col">
+                  <h5>Categories</h5>
+
+                   <ul class="grid_3">
+                      <li><a href="">Dresses</a></li>
+                      <li><a href="">Shirts &amp; Tops</a></li>
+                      <li><a href="">Knits &amp; Tees</a></li>
+                      <li><a href="">Sweaters</a></li>
+                      <li><a href="">Blazers &amp; Jackets</a></li>
+                      <li><a href="">Outerwear</a></li>
+                      <li><a href="">Pants</a></li>
+                   </ul>
+
+                   <ul class="grid_2">
+                      <li><a href="">Skirts</a></li>
+                      <li><strong><a href="">Shop All</a></strong></li>
+                   </ul>
+                </div><!-- end .grid_6 -->
+                <div class="clear">&nbsp;</div>
+
+            </div><!--/.dropdown-->
+          </li>
+          <li class="break_backslash">/</li>
+          <li id="nav_men" class="drop">
+            <a href="">Men</a>
+             <div class="dropdown">
+
+                <div class="clear">&nbsp;</div>
+
+            </div><!--/.dropdown-->
+          </li>
+          <li class="break_backslash">/</li>
+          <li id="nav_theory" class="drop">
+            <a href="">Inside Theory</a>
+            <div class="dropdown">
+
+                <div class="clear">&nbsp;</div>
+
+            </div><!--/.dropdown-->
+          </li>
+          <li class="break_backslash">/</li>
+          <li id="nav_search" class="">
+            <a>
+              <span id="search_txt">Search</span> 
+              <div id="global_search" style="display: none;">
+                <input id="global_search_txt" type="text"/>
+              </div><!--/global_search-->
+              <i class="icon_search"></i>
+            </a>
+          </li>
+        </ul><!--/main_nav-->
+
+        <div class="clear">&nbsp;</div>
+
+    </nav>
